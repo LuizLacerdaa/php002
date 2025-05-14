a3bc047668f8ae7b08e58deb458c68847963ded4
LuizLacerdaa <luizlacerda.r@gmail.com>
2025-05-14 20:29:31 -0300

    Criação de login e senha

---
+++ bloqueado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BLOQUEADO</title>
+</head>
+
+<body style="background-color: red;">
+    <img src="negado.png" alt="acesso bloqueado">
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form name="form-login" method="post" action="fazer-login.php">
+        Usuario: <input type="text" name="usuario"> <br>
+        Senha: <input type="password" name="senha"> <br>
+        <button type="submit">Acessar Sistema</button>
+    </form>
+</body>
+
+
+</html>
